9088c682af1ff768d587ceee303e19da856024f8
JINU-CHANG <jenny010925@naver.com>
2023-04-15 17:48:55 +0900

    initial commit

---
+++ src/main/java/com/kusitms/hotsixServer/HotsixServerApplication.java
@@ -0,0 +1,13 @@
+package com.kusitms.hotsixServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotsixServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotsixServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusitms/hotsixServer/HotsixServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.hotsixServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotsixServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
