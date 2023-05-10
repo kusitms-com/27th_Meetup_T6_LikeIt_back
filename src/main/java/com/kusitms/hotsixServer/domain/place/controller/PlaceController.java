46c51493d7ea736b4597b9d43f25e4470be9159e
subin1107 <btsiu@naver.com>
2023-05-10 18:07:40 +0900

    [REFACTOR] 장소 관련 API RESPONSE 형식 통일

--- src/main/java/com/kusitms/hotsixServer/domain/place/controller/PlaceController.java
+++ src/main/java/com/kusitms/hotsixServer/domain/place/controller/PlaceController.java
@@ -29,21 +29,21 @@ public class PlaceController {
 
     @ApiOperation("카테고리1(음식점, 카페, 놀거리) 선택시 장소 출력 코드")
     @GetMapping("/list/{category1Id}")
-    public ResponseEntity<Category1Response> getPlacesByCategory1(@PathVariable("category1Id") Long category1Id) {
+    public ResponseEntity<ResponseDto<Category1Response>> getPlacesByCategory1(@PathVariable("category1Id") Long category1Id) {
         Category1Response placeResponse = placeCategoryService.getPlacesByCategory1Response(category1Id);
 
         return ResponseEntity.ok(ResponseDto.create(
                 PlaceConstants.EBoardResponseMessage.RESPONSE_PLACE_CATEGORY1_SUCCESS.getMessage(),
-                placeResponse).getData());
+                placeResponse));
     }
 
     @ApiOperation("장소 상세 정보 조회")
     @GetMapping("/detail/{placeId}")
-    public ResponseEntity<PlaceDetail> getPlaceDetail(@PathVariable("placeId") Long placeId) {
+    public ResponseEntity<ResponseDto<PlaceDetail>> getPlaceDetail(@PathVariable("placeId") Long placeId) {
         PlaceDetail placeDetail = placeDetailService.getPlaceDetail(placeId);
 
         return ResponseEntity.ok(ResponseDto.create(
                 PlaceConstants.EBoardResponseMessage.RESPONSE_PLACE_DETAIL_SUCCESS.getMessage(),
-                placeDetail).getData());
+                placeDetail));
     }
 }
