43414b8ebfd2b347f1a56284feb36349b41cb39e
subin1107 <btsiu@naver.com>
2023-05-11 04:39:15 +0900

    [FEAT] 유저 필터 관련순 장소 출력 기능 구현

--- src/main/java/com/kusitms/hotsixServer/domain/place/repository/PlaceFilterRepository.java
+++ src/main/java/com/kusitms/hotsixServer/domain/place/repository/PlaceFilterRepository.java
@@ -1,13 +1,18 @@
 package com.kusitms.hotsixServer.domain.place.repository;
 
+import com.kusitms.hotsixServer.domain.place.entity.Place;
 import com.kusitms.hotsixServer.domain.place.entity.PlaceFilter;
+import com.kusitms.hotsixServer.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PlaceFilterRepository extends JpaRepository<PlaceFilter, Long> {
-    List<PlaceFilter> findByPlaceIdOrderByCountDesc(Long placeId);
-
+    //사용자 선택 필터값과 제일 관련도 높은 데이터부터 출력
+    @Query("SELECT pf.place FROM PlaceFilter pf JOIN pf.filter f JOIN pf.place p JOIN p.category1 c1 WHERE f IN (SELECT uf.filter FROM UserFilter uf WHERE uf.user = :user) AND c1.id = :category1Id GROUP BY pf.place ORDER BY COUNT(pf.place) DESC")
+    List<Place> findAllByUserAndCategory1(@Param("user") User user, @Param("category1Id") Long category1Id);
 }
--- src/main/java/com/kusitms/hotsixServer/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/kusitms/hotsixServer/domain/review/repository/ReviewRepository.java
@@ -1,10 +1,18 @@
 package com.kusitms.hotsixServer.domain.review.repository;
 
+import com.kusitms.hotsixServer.domain.place.entity.Place;
 import com.kusitms.hotsixServer.domain.review.entity.Review;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByPlaceId(Long placeId);
+    @Query("SELECT rs.sticker.name, COUNT(rs) AS stickerCount FROM ReviewSticker rs WHERE rs.review.place = :place GROUP BY rs.sticker ORDER BY stickerCount DESC")
+    List<Object[]> findTopStickersByPlace(@Param("place") Place place, Pageable pageable);
 }
+
