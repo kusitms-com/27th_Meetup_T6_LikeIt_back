6530e246ad08ad609e7dc653ad5632cd0a93eec5
JINU-CHANG <jenny010925@naver.com>
2023-05-12 15:40:08 +0900

    [FIX] 오류 확인

--- src/main/java/com/kusitms/hotsixServer/SampleController/SampleController.java
+++ src/main/java/com/kusitms/hotsixServer/SampleController/SampleController.java
@@ -1,13 +1,16 @@
 package com.kusitms.hotsixServer.SampleController;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SampleController {
 
     @GetMapping(value = "/sample")
     public String sample(){
+        log.info("test");
         return "hello springboot!" ;
     }
 }
