60fd1bf3a012cacb1bd63110f141a9e06e3e9b19
JINU-CHANG <jenny010925@naver.com>
2023-05-21 22:42:01 +0900

    [FEAT] 스티커 이미지 추가

--- src/main/java/com/kusitms/hotsixServer/SampleController/SampleController.java
+++ src/main/java/com/kusitms/hotsixServer/SampleController/SampleController.java
@@ -1,16 +1,29 @@
 package com.kusitms.hotsixServer.SampleController;
 
+import com.kusitms.hotsixServer.global.config.S3UploadUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 public class SampleController {
+
+    private final S3UploadUtil s3UploadUtil;
 
     @GetMapping(value = "/sample")
     public String sample(){
         log.info("ci/cd 시간 체크");
         return "hello springboot!" ;
+    }
+
+    @PostMapping(value = "/stickers")
+    public String  stickers(@RequestPart(value = "file", required = false) MultipartFile multipartFile){
+        return s3UploadUtil.upload(multipartFile, "sticker");
     }
 }
